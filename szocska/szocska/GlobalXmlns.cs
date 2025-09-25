10e7b0ee4f4157473af4e45f33f6a6fadde6c9bf
HydraC0DE <szaboakos0409@gmail.com>
2025-09-25 17:36:28 +0200

    gitignore and empty project

---
+++ szocska/szocska/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "szocska")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "szocska.Pages")]
